ba7436ca919675009ca2a5c2176364552cca4809
PerPlayer <1610719997@qq.com>
2019-03-05 00:06:04 +0800

    init

--- src/main/java/com/crawler/CrawlerApplication.java
+++ src/main/java/com/crawler/CrawlerApplication.java
@@ -1,15 +1,19 @@
 package com.crawler;
 
+import com.crawler.config.MybatisConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 //@Profile("dev")
-public class CrawlerApplication implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+public class CrawlerApplication extends SpringBootServletInitializer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
     public static void main(String[] args) {
         SpringApplication.run(CrawlerApplication.class, args);
@@ -18,5 +22,11 @@ public class CrawlerApplication implements WebServerFactoryCustomizer<Configurab
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
         factory.setPort(80);
+    }
+
+    //可执行war包
+    @Override
+    public SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CrawlerApplication.class);
     }
 }
